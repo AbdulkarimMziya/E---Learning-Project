eb69c92f9436133b014f6518239c637926013365
AbdulkarimMziya <abdulmziya@gmail.com>
2024-03-05 17:55:11 -0800

    --Setup firebase
    Connected backed with front end
    
    Co-Authored-By: Oloyededaniel <90796979+Oloyededaniel@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello Webpack")
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDvHWlJ7txJ8fTu1afqDwygk9v5ifh-9AE",
+    authDomain: "e-learning-web-c83f9.firebaseapp.com",
+    projectId: "e-learning-web-c83f9",
+    storageBucket: "e-learning-web-c83f9.appspot.com",
+    messagingSenderId: "577791964756",
+    appId: "1:577791964756:web:7716600e9e4580690e5fc1"
+  };
+
+  // initialize Firebase
+  initializeApp(firebaseConfig);
