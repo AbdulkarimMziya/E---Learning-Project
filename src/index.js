9c4682dba0880c0ca69d0e193d6dd323e9dfd5ce
AbdulkarimMziya <abdulmziya@gmail.com>
2024-03-06 20:01:01 -0800

    --form setups
    Co-Authored-By: Oloyededaniel <90796979+Oloyededaniel@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import { initializeApp } from 'firebase/app';
 
+import {
+  getDatabase,
+  set,
+  ref,
+  child,
+  get
+} from 'firebase/database'
+
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
+
 const firebaseConfig = {
     apiKey: "AIzaSyDvHWlJ7txJ8fTu1afqDwygk9v5ifh-9AE",
     authDomain: "e-learning-web-c83f9.firebaseapp.com",
@@ -7,7 +21,74 @@ const firebaseConfig = {
     storageBucket: "e-learning-web-c83f9.appspot.com",
     messagingSenderId: "577791964756",
     appId: "1:577791964756:web:7716600e9e4580690e5fc1"
-  };
+};
 
-  // initialize Firebase
-  initializeApp(firebaseConfig);
+// initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// initialize Services
+const db = getDatabase();
+const auth = getAuth(app);
+const dbref = ref(db);
+
+
+// Signup Form
+let signupForm = document.getElementById('signup-form');
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let EmailIn = document.getElementById('signup-email');
+let PasswordIn = document.getElementById('signup-password');
+
+let RegisterUser = evt => {
+  evt.preventDefault();
+
+  createUserWithEmailAndPassword(auth, EmailIn.value, PasswordIn.value)
+  .then((cred) => {
+    // console.log(cred.user);
+    set(ref(db, 'UserAuthist/' + cred.user.uid),{
+      firstname: firstName.value,
+      lastname: lastName.value
+    })
+    // *reset values
+  })
+  .catch((error) => {
+    alert(error.message)
+    console.log(error.message)
+    console.log(error.code)
+  })
+
+}
+
+signupForm.addEventListener('submit', RegisterUser);
+
+
+// Login Form
+let loginForm = document.getElementById('login-form');
+let LoginUser = evt => {
+  evt.preventDefault();
+
+  signInWithEmailAndPassword(auth, EmailIn.value, PasswordIn.value)
+  .then((cred) => {
+    // console.log(cred.user);
+    get(child(dbref, 'UserAuthList/' + cred.user.uid)).then((snapshot) =>{
+      if(snapshot.exists){
+        sessionStorage.setItem("user-info", JSON.stringify({
+          firstname: snapshot.val().firstName,
+          lastname: snapshot.val().lastName
+        }))
+        sessionStorage.setItem("user-creds", JSON.stringify(cred.user));
+        window.location.href = "index.html";
+      }
+    })
+
+    // *reset values
+  })
+  .catch((error) => {
+    alert(error.message)
+    console.log(error.message)
+    console.log(error.code)
+  })
+}
+
+loginForm.addEventListener('submit', LoginUser);
+
