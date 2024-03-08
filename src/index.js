1714a3625e8d422baf361aa666089911a5a7aa18
AbdulkarimMziya <abdulmziya@gmail.com>
2024-03-08 12:16:09 -0800

    -update forms
    Co-Authored-By: Oloyededaniel <90796979+Oloyededaniel@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ import {
 
 import { 
   getAuth,
+  onAuthStateChanged,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
 } from 'firebase/auth';
@@ -30,47 +31,60 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase();
 const auth = getAuth(app);
 const dbref = ref(db);
+// Check user authentication state
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    const uid = user.uid;
+    document.getElementById('signing').style.display = 'none';
+    document.getElementById('logout').style.display = 'block';
+    console.log('User is logged in')
+  } else {
+    // User is signed out
+    document.getElementById('logout').style.display = 'none';
+    console.log('User is not logged in!!!')
+  }
+});
 
 
 // Signup Form
-let signupForm = document.getElementById('signup-form');
-let firstname = document.getElementById('first-name');
-let lastname = document.getElementById('last-name');
-let EmailIn = document.getElementById('signup-email');
-let PasswordIn = document.getElementById('signup-password');
-
-let RegisterUser = evt => {
+document.getElementById("signup-form").addEventListener("click", function(evt) {
   evt.preventDefault();
 
-  createUserWithEmailAndPassword(auth, EmailIn.value, PasswordIn.value)
-  .then((cred) => {
-    // console.log(cred.user);
-    set(ref(db, 'UserAuthist/' + cred.user.uid),{
-      firstname: firstname.value,
-      lastname: lastname.value
-    })
-    // *reset values
-    alert('User Registered...')
+  var email =  document.getElementById("signup-email").value;
+  var password = document.getElementById("signup-password").value;
+  //For new registration
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    console.log(user);
+    alert("Registration successfully!!");
+    // ...
   })
   .catch((error) => {
-    alert(error.message)
-    console.log(error.message)
-    console.log(error.code)
-  })
-
-}
-
-signupForm.addEventListener('submit', RegisterUser);
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    console.log(errorMessage);
+    alert(error);
+  });		  		  
+});
 
 
 // Login Form
-let loginForm = document.getElementById('login-form');
-let LoginUser = evt => {
+document.getElementById("login-form").addEventListener("submit", function(evt) {
   evt.preventDefault();
 
-  signInWithEmailAndPassword(auth, EmailIn.value, PasswordIn.value)
+  let email = document.getElementById('login-email').value
+  let password = document.getElementById('login-password').value
+
+  signInWithEmailAndPassword(auth, email, password)
   .then((cred) => {
     // console.log(cred.user);
+    alert(cred.user.email +" Login successfully!!!");
+    
+    window.location.href = "index.html";
     get(child(dbref, 'UserAuthList/' + cred.user.uid)).then((snapshot) =>{
       if(snapshot.exists){
         sessionStorage.setItem("user-info", JSON.stringify({
@@ -78,9 +92,10 @@ let LoginUser = evt => {
           lastname: snapshot.val().lastname
         }))
         sessionStorage.setItem("user-creds", JSON.stringify(cred.user));
-        window.location.href = "index.html";
       }
     })
+    // Update the welcome message with user information
+
     // *reset values
   })
   .catch((error) => {
@@ -88,7 +103,20 @@ let LoginUser = evt => {
     console.log(error.message)
     console.log(error.code)
   })
-}
+});
 
-loginForm.addEventListener('submit', LoginUser);
 
+// Logout   
+document.getElementById("logout").addEventListener("click", function() {
+  signOut(auth).then(() => {
+    // Sign-out successful.
+    console.log('Sign-out successful.');
+    alert('Sign-out successful.');
+    // Redirect the user to the login page or any other desired page
+    document.getElementById('logout').style.display = 'none';
+  }).catch((error) => {
+    // An error happened.
+    console.error('Error during sign-out:', error);
+    alert('An error occurred during sign-out. Please try again later.');
+  });		  		  
+});
